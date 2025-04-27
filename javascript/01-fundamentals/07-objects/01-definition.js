5dafb37bd60250babe4542f3c89e1329d60010da
lar1ck <shyakangango77@gmail.com>
2025-04-27 23:35:20 +0200

    object def

---
+++ javascript/01-fundamentals/07-objects/01-definition.js
@@ -0,0 +1,12 @@
+// Definition of an Object
+// An object is a fundamental data type in JavaScript that stores key-value pairs.
+// It groups multiple values together — both data (state) and functions (behavior).
+
+// Example:
+const dog = {
+    name: 'Max',
+    age: 5
+  };
+  
+  console.log(dog);
+  
